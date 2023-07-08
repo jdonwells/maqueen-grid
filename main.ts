886fb98769d80de7d90b144e82701a86a7c91cf7
Don Wells <jdonwells@gmail.com>
2023-07-08 16:17:01 -0400

    added map

--- main.ts
+++ main.ts
@@ -1,8 +1,5 @@
 function show_line_sensors () {
     basic.clearScreen()
-    if (maqueenPlusV2.readLineSensorState(maqueenPlusV2.MyEnumLineSensor.SensorR2) == 1) {
-        led.plot(0, 2)
-    }
     if (maqueenPlusV2.readLineSensorState(maqueenPlusV2.MyEnumLineSensor.SensorR1) == 1) {
         led.plot(1, 2)
     }
@@ -12,23 +9,27 @@ function show_line_sensors () {
     if (maqueenPlusV2.readLineSensorState(maqueenPlusV2.MyEnumLineSensor.SensorL1) == 1) {
         led.plot(3, 2)
     }
-    if (maqueenPlusV2.readLineSensorState(maqueenPlusV2.MyEnumLineSensor.SensorL2) == 1) {
-        led.plot(4, 2)
-    }
 }
 function on_crossroad () {
     return maqueenPlusV2.readLineSensorState(maqueenPlusV2.MyEnumLineSensor.SensorL1) == ON && maqueenPlusV2.readLineSensorState(maqueenPlusV2.MyEnumLineSensor.SensorR1) == ON
 }
+function initialize_test_turns () {
+    test_turns = [
+    STRAIGHT,
+    RIGHT,
+    RIGHT,
+    STRAIGHT,
+    RIGHT,
+    RIGHT
+    ]
+    next_turn = 0
+}
 function center_on_crossroad () {
-    start_time = input.runningTime()
-    iterations = 0
-    for (let index = 0; index < crossroad_pause / ms_per_iteration; index++) {
-        iterations += 1
+    for (let index = 0; index < iterations_to_center_of_line; index++) {
         drive_mostly_straight()
         basic.pause(1)
     }
-    stop()
-    radio.sendNumber((input.runningTime() - start_time) / iterations)
+    maqueenPlusV2.controlMotorStop(maqueenPlusV2.MyEnumMotor.AllMotor)
 }
 function make_a_90_degree_turn (direction: number) {
     maqueenPlusV2.showColor(maqueenPlusV2.NeoPixelColors.Yellow)
@@ -39,10 +40,8 @@ function make_a_90_degree_turn (direction: number) {
         maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.LeftMotor, maqueenPlusV2.MyEnumDir.Backward, spin_speed)
         maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.RightMotor, maqueenPlusV2.MyEnumDir.Forward, spin_speed)
     }
-    if (on_line()) {
-        while (on_line()) {
-            basic.pause(1)
-        }
+    while (on_line()) {
+        basic.pause(1)
     }
     while (!(on_line())) {
         basic.pause(1)
@@ -54,32 +53,48 @@ function stop () {
     go = false
     maqueenPlusV2.showColor(maqueenPlusV2.NeoPixelColors.Red)
 }
+input.onButtonPressed(Button.A, function () {
+    led.unplot(x, y)
+    x = (x + 1) % 3
+    led.plot(x, y)
+})
+function set_implied_walls () {
+    for (let column of map) {
+        column[0] = add_wall(column[0], NORTH)
+        column[4] = add_wall(column[4], SOUTH)
+    }
+    for (let index = 0; index <= 4; index++) {
+        map[0][index] = add_wall(map[0][index], WEST)
+        map[2][index] = add_wall(map[2][index], EAST)
+    }
+}
 function on_line () {
     return maqueenPlusV2.readLineSensorState(maqueenPlusV2.MyEnumLineSensor.SensorL1) == ON || (maqueenPlusV2.readLineSensorState(maqueenPlusV2.MyEnumLineSensor.SensorR1) == ON || maqueenPlusV2.readLineSensorState(maqueenPlusV2.MyEnumLineSensor.SensorM) == ON)
 }
 function initialize_constants () {
-    crossroad_pause = 330
     ON = 1
     spin_speed = 40
-    degrees_per_second = 110
     RIGHT = 1
-    CENTER = 0
+    STRAIGHT = 0
     LEFT = -1
     wheel_speed = 60
-    Kp = 0.5
+    Kp = 0.4
     wheel_bias = 1.05
-    WHITE = 230
-    ms_per_iteration = 5
+    iterations_to_center_of_line = 66
 }
 function adjust_stop_point (received_message: string) {
     if (received_message.compare("A") == 0) {
-        crossroad_pause += 5
-        radio.sendNumber(crossroad_pause)
+        iterations_to_center_of_line += 1
+        radio.sendNumber(iterations_to_center_of_line)
     } else if (received_message.compare("B") == 0) {
-        crossroad_pause += -5
-        radio.sendNumber(crossroad_pause)
+        iterations_to_center_of_line += -1
+        radio.sendNumber(iterations_to_center_of_line)
     }
 }
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("" + (map[x][y]))
+    led.plot(x, y)
+})
 radio.onReceivedString(function (receivedString) {
     if (receivedString.compare("C") == 0) {
         stop()
@@ -90,8 +105,13 @@ radio.onReceivedString(function (receivedString) {
     } else if (receivedString.compare("D") == 0) {
         make_a_90_degree_turn(RIGHT)
     } else {
-        adjust_stop_point(receivedString)
+    	
     }
+})
+input.onButtonPressed(Button.B, function () {
+    led.unplot(x, y)
+    y = (y + 1) % 5
+    led.plot(x, y)
 })
 function drive_mostly_straight () {
     maqueenPlusV2.showColor(maqueenPlusV2.NeoPixelColors.Green)
@@ -102,7 +122,7 @@ function drive_mostly_straight () {
         maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.RightMotor, maqueenPlusV2.MyEnumDir.Forward, wheel_speed - wheel_delta)
     } else {
         maqueenPlusV2.showColor(maqueenPlusV2.NeoPixelColors.Yellow)
-        if (wheel_delta >= 0) {
+        if (wheel_delta > 0) {
             maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.LeftMotor, maqueenPlusV2.MyEnumDir.Forward, Math.min(wheel_speed + wheel_delta, 255))
             maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.RightMotor, maqueenPlusV2.MyEnumDir.Backward, Math.min(Math.abs(wheel_speed - wheel_delta), 255))
         } else {
@@ -111,35 +131,95 @@ function drive_mostly_straight () {
         }
     }
 }
+function initialize_map () {
+    UNKNOWN = "?"
+    WALL = "X"
+    OPEN = "_"
+    BLANK = "" + UNKNOWN + UNKNOWN + UNKNOWN + UNKNOWN
+    map = [[
+    BLANK,
+    BLANK,
+    BLANK,
+    BLANK,
+    BLANK
+    ], [
+    BLANK,
+    BLANK,
+    BLANK,
+    BLANK,
+    BLANK
+    ], [
+    BLANK,
+    BLANK,
+    BLANK,
+    BLANK,
+    BLANK
+    ]]
+    NORTH = 0
+    EAST = 1
+    SOUTH = 2
+    WEST = 3
+    opposite_direction = [
+    SOUTH,
+    WEST,
+    NORTH,
+    EAST
+    ]
+    set_implied_walls()
+    x = 0
+    y = 0
+    led.plot(x, y)
+}
+function add_wall (walls: string, direction: number) {
+    separate_walls = walls.split("")
+    separate_walls[direction] = WALL
+    return "" + separate_walls[NORTH] + separate_walls[EAST] + separate_walls[SOUTH] + separate_walls[WEST]
+}
+function make_a_turn () {
+    if (test_turns[next_turn] == LEFT || test_turns[next_turn] == RIGHT) {
+        make_a_90_degree_turn(test_turns[next_turn])
+    }
+    next_turn = (next_turn + 1) % test_turns.length
+}
+let separate_walls: string[] = []
+let opposite_direction: number[] = []
+let BLANK = ""
+let OPEN = ""
+let WALL = ""
+let UNKNOWN = ""
 let wheel_delta = 0
 let error = 0
-let WHITE = 0
 let wheel_bias = 0
 let Kp = 0
 let wheel_speed = 0
 let LEFT = 0
-let CENTER = 0
-let RIGHT = 0
-let degrees_per_second = 0
+let EAST = 0
+let WEST = 0
+let SOUTH = 0
+let NORTH = 0
+let map: string[][] = []
+let y = 0
+let x = 0
 let go = false
 let spin_speed = 0
-let ms_per_iteration = 0
-let crossroad_pause = 0
-let iterations = 0
-let start_time = 0
+let iterations_to_center_of_line = 0
+let next_turn = 0
+let RIGHT = 0
+let STRAIGHT = 0
+let test_turns: number[] = []
 let ON = 0
-maqueenPlusV2.I2CInit()
 radio.setGroup(42)
 initialize_constants()
+initialize_map()
+initialize_test_turns()
 stop()
-basic.showIcon(IconNames.Heart)
 basic.forever(function () {
     if (go) {
         if (on_crossroad()) {
             center_on_crossroad()
+            make_a_turn()
         } else {
             drive_mostly_straight()
         }
     }
-    show_line_sensors()
 })
